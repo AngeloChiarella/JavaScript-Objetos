16914748d9e078f4af98d80f0c7ab1671ddb9d4f
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-22 10:01:31 -0300

    Manipulacao de Objetos

---
+++ aula2/lista-objetos.js
@@ -0,0 +1,30 @@
+// aninhado - um dentro do outro
+
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["115555550", "114444440"]
+};
+
+cliente.enderecos = [
+    {
+        rua: "R. Joseph Climber",
+        numero: 1337,
+        apartamento: true,
+        complemento: "ap 934"
+    }
+];
+
+cliente.enderecos.push({
+    rua: "R. Joseph Ladder",
+    numero: 404,
+    apartamento: false
+})
+
+const listaApenasApartamentos = cliente.enderecos.filter(
+    (endereco) => endereco.apartamento === true
+)
+
+// console.log(cliente.enderecos)
+console.log(listaApenasApartamentos)
